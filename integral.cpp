41333dd8a80c0a5f566d7f0106653e7bda935095
Triach Rold <156170660+triach-rold@users.noreply.github.com>
2024-05-20 09:53:49 +0530

    Update integral.cpp

--- integral.cpp
+++ integral.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <cctype>
@@ -16,6 +16,7 @@ public:
 private:
     std::string expr;
     size_t pos;
+
     double parseExpression() {
         double result = parseTerm();
         while (pos < expr.size()) {
@@ -60,7 +61,6 @@ private:
         }
         return result;
     }
-
     double parsePrimary() {
         if (expr[pos] == '(') {
             pos++;
@@ -79,11 +79,15 @@ private:
     }
     double parseNumber() {
         size_t startPos = pos;
+        if (expr[pos] == '-') {
+            pos++;
+        }
         while (pos < expr.size() && (isdigit(expr[pos]) || expr[pos] == '.')) {
             pos++;
         }
         return std::stod(expr.substr(startPos, pos - startPos));
     }
+
     double parseFunction() {
         std::string func;
         while (pos < expr.size() && isalpha(expr[pos])) {
@@ -111,6 +115,7 @@ private:
             if (func == "sqrt") return sqrt(arg);
             if (func == "abs") return fabs(arg);
             if (func == "exp") return exp(arg);
+            if (func == "sgn") return (arg > 0) - (arg < 0);
 
             throw std::runtime_error("Unknown function: " + func);
         }
@@ -130,26 +135,60 @@ double evaluate_function(const std::string& expression, double x) {
     Parser parser(replacedExpression);
     return parser.parse();
 }
+double simpsons_rule(const std::string& expression, double lower_bound, double upper_bound, int n) {
+    if (n % 2 != 0) {
+        throw std::invalid_argument("Simpson's rule requires an even number of intervals.");
+    }
+    double h = (upper_bound - lower_bound) / n;
+    double sum = evaluate_function(expression, lower_bound) + evaluate_function(expression, upper_bound);
+
+    for (int i = 1; i < n; i++) {
+        double x = lower_bound + i * h;
+        if (i % 2 == 0) {
+            sum += 2 * evaluate_function(expression, x);
+        } else {
+            sum += 4 * evaluate_function(expression, x);
+        }
+    }
+    return sum * h / 3;
+}
 int main(int argc, char* argv[]) {
-    if (argc != 4 && argc != 6) {
-       std::cerr << "\033[1;31mUsage:\033[0m " << argv[0] << " ./executable_name \"function\" (lower_bound) (upper_bound) --increment (increment_number)" << std::endl;
-       std::cout << "\033[1;36mNote - the increment flag is optional - it is set to 1000000 by default. Bigger numbers make the result more accurate, but evaluation time will also increase.\033[0m" << std::endl;
+    if (argc != 4 && argc != 6 && argc != 7) {
+        std::cerr << "\033[1;31mUsage:\033[0m " << argv[0] << " \"function\" (lower_bound) (upper_bound) [--increment (increment_number)] [--simpson]" << std::endl;
+        std::cout << "\033[1;36mNote: The increment flag is optional and set to 1000000 by default. Bigger numbers make the result more accurate, but evaluation time will also increase.\033[0m" << std::endl;
         return 1;
     }
     std::string function = argv[1];
     double lower_bound = std::stod(argv[2]);
     double upper_bound = std::stod(argv[3]);
-    double inf = 1000000; // default increment value
-    // increment "flag" trigger
-    if (argc == 6 && std::string(argv[4]) == "--increment") {
-        inf = std::stod(argv[5]);
+    double increment = 1000000; // default increment value
+    bool use_simpsons = false;
+
+    for (int i = 4; i < argc; i++) {
+        if (std::string(argv[i]) == "--increment" && i + 1 < argc) {
+            increment = std::stod(argv[++i]);
+        } else if (std::string(argv[i]) == "--simpson") {
+            use_simpsons = true;
+        }
     }
-    double h = (upper_bound - lower_bound) / inf;
-    double sum = 0;
-    for (int i = 0; i <= inf; i++) {
-        double x = lower_bound + (i * h);
-        sum += evaluate_function(function, x) * h;
+    try {
+        if (use_simpsons) {
+            double result = simpsons_rule(function, lower_bound, upper_bound, static_cast<int>(increment));
+            std::cout << "Result by simpson's rule is " << result << std::endl;
+        } else {
+            double h = (upper_bound - lower_bound) / increment;
+            double sum = 0;
+
+            for (int i = 0; i <= increment; i++) {
+                double x = lower_bound + (i * h);
+                sum += evaluate_function(function, x) * h;
+            }
+            std::cout << "Result of the integral is " << sum << std::endl;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "\033[1;31mError:\033[0m " << e.what() << std::endl;
+        return 1;
     }
-    std::cout << "Result of the integral is " << sum << std::endl;
+
     return 0;
 }
